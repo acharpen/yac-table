dba7fdcbe2e86f97f59bb4b0c820bb4259543327
Alan Charpentier <alan.charpentier@gmail.com>
2020-10-26 22:27:03 +0100

    update test

--- src/__tests__/list-table.test.ts
+++ src/__tests__/list-table.test.ts
@@ -1,5 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+
 import { TestObject, defaultFormatter } from './setup';
 import { ListTable } from '../list-table';
+import { getByText } from '@testing-library/dom';
 
 const columnOptions = [
   { align: 'left' as const, field: 'col1' as const, formatter: defaultFormatter, sortFeature: true, title: 'col1' },
@@ -13,7 +16,12 @@ const table = new ListTable<TestObject>(document.body, { columnOptions, tableOpt
 
 describe('checking initialization', () => {
   test('table is empty', () => {
-    expect(table.getNodes().length).toBe(0);
+    expect(table.getNodes()).toHaveLength(0);
+  });
+
+  test('table header is correct', () => {
+    expect(getByText(document.body, 'col1')).toBeTruthy();
+    expect(getByText(document.body, 'col2')).toBeTruthy();
   });
 });
 
@@ -26,6 +34,26 @@ describe('checking setData', () => {
   });
 
   test('table has 2 nodes', () => {
-    expect(table.getNodes().length).toBe(2);
+    expect(table.getNodes()).toHaveLength(2);
+  });
+});
+
+describe('checking addData', () => {
+  beforeEach(() => {
+    table.addData({ col1: 'value21', col2: 'value22' }, { position: 'top' });
+  });
+
+  test('table has 3 nodes', () => {
+    expect(table.getNodes()).toHaveLength(3);
+  });
+});
+
+describe('checking deleteNodes', () => {
+  beforeEach(() => {
+    table.deleteNodes([0]);
+  });
+
+  test('table has 2 nodes', () => {
+    expect(table.getNodes()).toHaveLength(2);
   });
 });
