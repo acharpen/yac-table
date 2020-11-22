a97e948aa49c7f002e598390ab018c2927151dc3
Alan Charpentier <alan.charpentier@gmail.com>
2020-11-22 15:28:45 +0100

    remove rollup multi-entry plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,18 @@
-import multi from '@rollup/plugin-multi-entry';
 import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
 
 export default {
-  input: ['./src/list-table.ts', './src/tree-table.ts'],
+  input: 'src/index.ts',
   output: {
-    dir: './lib',
+    dir: 'lib',
     format: 'es'
   },
   plugins: [
-    multi({
-      entryFileName: 'index.js'
-    }),
     typescript({
       declaration: true,
-      declarationDir: './lib',
+      declarationDir: 'lib',
       removeComments: true,
-      rootDir: './src'
+      rootDir: 'src'
     }),
     terser()
   ]
