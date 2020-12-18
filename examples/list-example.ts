4ad43d488b57954fe6160c17aefde32ab2378123
Alan Charpentier <alan.charpentier@gmail.com>
2020-12-18 10:40:29 +0100

    update examples

--- examples/list-example.ts
+++ examples/list-example.ts
@@ -1,7 +1,13 @@
 import { ExampleObject, columnOptions, tableOptions } from './setup';
 import { ListTable } from '../src/list-table';
 import { Node } from '../src/node';
-import { listData } from './data';
+
+const listData = [...Array(1e5).keys()].map((i) => ({
+  col1: `value${i}1`,
+  col2: `value${i}2`,
+  col3: `value${i}3`,
+  col4: `value${i}4`
+}));
 
 const tableContainerElt = document.getElementById('table') as HTMLElement;
 const table = new ListTable<ExampleObject>(tableContainerElt, { columnOptions, tableOptions });
--- examples/tree-example.ts
+++ examples/tree-example.ts
@@ -1,7 +1,45 @@
 import { ExampleObject, columnOptions, tableOptions } from './setup';
 import { Node } from '../src/node';
 import { TreeTable } from '../src/tree-table';
-import { treeData } from './data';
+
+const treeData = [...Array(1e5).keys()].map((i) => ({
+  children: [
+    {
+      children: [
+        {
+          children: [],
+          value: {
+            col1: `childchildvalue${i}11`,
+            col2: `childchildvalue${i}12`,
+            col3: `childchildvalue${i}13`,
+            col4: `childchildvalue${i}14`
+          }
+        }
+      ],
+      value: {
+        col1: `childvalue${i}11`,
+        col2: `childvalue${i}12`,
+        col3: `childvalue${i}13`,
+        col4: `childvalue${i}14`
+      }
+    },
+    {
+      children: [],
+      value: {
+        col1: `childvalue${i}21`,
+        col2: `childvalue${i}22`,
+        col3: `childvalue${i}23`,
+        col4: `childvalue${i}24`
+      }
+    }
+  ],
+  value: {
+    col1: `value${i}1`,
+    col2: `value${i}2`,
+    col3: `value${i}3`,
+    col4: `value${i}4`
+  }
+}));
 
 const tableContainerElt = document.getElementById('table') as HTMLElement;
 const table = new TreeTable<ExampleObject>(tableContainerElt, {
