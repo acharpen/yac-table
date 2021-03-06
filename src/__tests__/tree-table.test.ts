b373d384d0a649b8bc19c5e43fbd5b8a3b34bada
Alan Charpentier <alan.charpentier@gmail.com>
2021-03-06 20:42:23 +0100

    fix tests

--- src/__tests__/tree-table.test.ts
+++ src/__tests__/tree-table.test.ts
@@ -5,8 +5,8 @@ import { TreeTable } from '../tree-table';
 import { getByText } from '@testing-library/dom';
 
 const columnOptions = [
-  { align: 'left' as const, formatter: defaultFormatter('col1'), id: 1, title: 'col1' },
-  { align: 'left' as const, formatter: defaultFormatter('col2'), id: 2, title: 'col2' }
+  { align: 'left' as const, formatter: defaultFormatter('col1'), id: 1, order: 1, title: 'col1' },
+  { align: 'left' as const, formatter: defaultFormatter('col2'), id: 2, order: 2, title: 'col2' }
 ];
 const tableOptions = { columnMinWidth: 40, nodeHeight: 40, visibleNodes: 10 };
 const table = new TreeTable<TestObject>(document.body, {
